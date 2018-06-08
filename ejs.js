ed0832d9830671214c7080891f8a0ac133108216
fjl <fengjianglin6817@dingtalk.com>
2018-06-08 11:54:31 +0800

    修改

--- ejs.js
+++ ejs.js
@@ -1,8 +1,3 @@
 const ejs=require('ejs');
-ejs.renderFile('./views/1.ejs',{names:'李四'},function (data,err) {
-    if (err){
-        console.log('编译失败');
-    } else {
-        console.log(data);
-    }
-});
+let str=ejs.renderFile('./views/1.ejs',{pretty:true});
+console.log(str);
--- jade.js
+++ jade.js
@@ -1,4 +1,4 @@
 const  jade=require('jade');
 //从文件里面读取
-let str=jade.renderFile('./views/1.jade',{pretty:true});
+let str=jade.renderFile('./views/2.jade',{pretty:true,name:'blue'});
 console.log(str);
